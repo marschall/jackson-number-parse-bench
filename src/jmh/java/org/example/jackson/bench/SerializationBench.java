9de116b1c5928632c83f0eadfaa6771b39640ffb
Philippe Marschall <philippe.marschall@gmail.com>
2022-12-28 23:14:34 +0100

    use pojo array

--- src/jmh/java/org/example/jackson/bench/SerializationBench.java
+++ src/jmh/java/org/example/jackson/bench/SerializationBench.java
@@ -3,6 +3,8 @@ package org.example.jackson.bench;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Param;
@@ -10,13 +12,14 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 
+import com.fasterxml.jackson.core.StreamWriteFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
 @State(Scope.Benchmark)
 public class SerializationBench {
 
-    private Pojo pojo;
+    private Pojo[] pojos;
 
     private Writer writer;
 
@@ -27,32 +30,36 @@ public class SerializationBench {
     private ObjectWriter fastObjectWriter;
     
     @Param({"true", "false"})
-    public boolean fast;
+    public boolean useFastWriter;
 
     @Setup
     public void setUp() {
-        this.pojo = Pojo.newRandomPojo(0L);
+        this.pojos = generatePojos(100);
         this.writer = new NullWriter();
         this.outputStream = new NullOutputStream();
-        this.defaultObjectWriter = new ObjectMapper()
-                                        .writerFor(Pojo.class);
-        this.fastObjectWriter = new ObjectMapper()
-//                                        .enable(com.fasterxml.jackson.core.StreamWriteFeature.USE_FAST_DOUBLE_WRITER)
-                                        .enable(com.fasterxml.jackson.core.JsonGenerator.Feature.USE_FAST_DOUBLE_WRITER)
-                                        .writerFor(Pojo.class);
+        this.defaultObjectWriter = new ObjectMapper().writerFor(Pojo[].class);
+        this.fastObjectWriter = this.defaultObjectWriter.with(StreamWriteFeature.USE_FAST_DOUBLE_WRITER);
+    }
+    
+    private static Pojo[] generatePojos(int size) {
+        List<Pojo> pojos = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            pojos.add(Pojo.newRandomPojo(i));
+        }
+        return pojos.toArray(new Pojo[0]);
     }
 
     @Benchmark
     public Writer writer() throws IOException {
-        ObjectWriter objectWriter = this.fast ? this.fastObjectWriter : this.defaultObjectWriter;
-        objectWriter.writeValue(this.writer, this.pojo);
+        ObjectWriter objectWriter = this.useFastWriter ? this.fastObjectWriter : this.defaultObjectWriter;
+        objectWriter.writeValue(this.writer, this.pojos);
         return this.writer;
     }
 
     @Benchmark
     public OutputStream outputStream() throws IOException {
-        ObjectWriter objectWriter = this.fast ? this.fastObjectWriter : this.defaultObjectWriter;
-        objectWriter.writeValue(this.outputStream, this.pojo);
+        ObjectWriter objectWriter = this.useFastWriter ? this.fastObjectWriter : this.defaultObjectWriter;
+        objectWriter.writeValue(this.outputStream, this.pojos);
         return this.outputStream;
     }
 
